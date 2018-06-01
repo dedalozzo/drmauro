1f20523d4ac402fde651f07e0a0cec6dd667c8b6
Filippo Fadda <filippo.fadda@gmail.com>
2018-06-01 01:48:25 +0100

    a lot of changes

--- drmauro.h
+++ drmauro.h
@@ -5,24 +5,39 @@
 #define COLONNE 8
 #define RIGHE_NO_MOSTRI 5
 
+#include <stdbool.h>
+
 enum contenuto { VUOTO, MOSTRO, PASTIGLIA };
 enum colore { ROSSO, GIALLO, BLU, NUMERO_COLORI };
 enum mossa { NONE, DESTRA, SINISTRA, GIU, ROT_DX, ROT_SX };
 enum stato { IN_CORSO, VITTORIA, SCONFITTA };
 enum senso { ORARIO, ANTIORARIO };
+enum orientamento { ORIZZONTALE, VERTICALE };
+
+
+struct pezzo {
+    int riga;
+    int colonna;
+    enum colore colore;
+};
+
+struct pastiglia {
+    enum orientamento orientamento;
+    struct pezzo pezzo1;
+    struct pezzo pezzo2;
+    bool attiva;
+};
 
 struct cella {
   enum contenuto tipo;
   enum colore colore;
-  /* ---- */
-  int id;
 };
 
 struct gioco {
   struct cella campo[RIGHE][COLONNE];
+  struct pastiglia pastiglia;
+  enum stato stato;
   int punti;
-  /* ---- */
-  int active_id;
 };
 
 
